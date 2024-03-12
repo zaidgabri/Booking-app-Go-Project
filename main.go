a1872f2ca1bcc26a8e866309b996ef0dcf14a415
zaid lagabri <zaidgabri976@gmail.com>
2024-03-12 09:31:45 +0000

    Getting User Input

--- main.go
+++ main.go
@@ -9,16 +9,24 @@ func main() {
 
 	fmt.Printf("conferenceTickets is %T , remainingTickets is %T , ocnferenceName is %T\n", confernceTicket, remainingTickets, conferncename)
 	fmt.Printf("Welcome to %v booking application\n", conferncename)
-	fmt.Printf("We have totla of %v tickets and %v are still av", confernceTicket, remainingTickets)
+	fmt.Printf("We have totla of %v tickets and %v are still av\n", confernceTicket, remainingTickets)
 	fmt.Println("Get your ticket  here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 	//ask suer for thier name
-
-	userName = "Tom"
+	fmt.Println("Enter your first Name")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your Last Name")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email Address")
+	fmt.Scan(&email)
+	fmt.Println("Enter Number of tickets")
+	fmt.Scan(&userTickets)
 	userTickets = 2
 
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	fmt.Printf(" Thank  you  %v %v for booking the tickets %v , You will  receive a confiramtaions email at %v ", firstName, lastName, userTickets, email)
 
 }
